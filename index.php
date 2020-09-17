9c571b48a96fcb391591c1ce7001fc646f0ca58e
pis0sion <zihuang2010@126.com>
2020-09-17 15:57:56 +0800

    format code

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
 
+use PhpOffice\PhpWord\Exception\CopyFileException;
+use PhpOffice\PhpWord\Exception\CreateTemporaryFileException;
 use Pis0sion\src\entity\GenerateWordDoc;
+use Pis0sion\src\service\CreateZipArchiveService;
 
 include_once "vendor/autoload.php";
 // 防止文件deleteBlock 和 cloneBlock 失效
@@ -785,24 +788,56 @@ $global_paras = [
     "body" => [],
 ];
 
-$word = new GenerateWordDoc();
-// 设置保存的路径
-$word->setSavePath(getcwd() . "//" . uniqid());
-// 生成各个接口的文档
-foreach ($list as $key => $parameter) {
-    $word->create($parameter, $key, $global_paras);
+
+(new WordService())->run($list, $global_paras);
+
+/**
+ * Class WordService
+ */
+class WordService
+{
+    /**
+     * @param array $list
+     * @param array $global_paras
+     * @throws Throwable
+     * @throws CopyFileException
+     * @throws CreateTemporaryFileException
+     */
+    public function run(array $list, array $global_paras)
+    {
+        $word = new GenerateWordDoc();
+        // 设置保存的路径
+        $word->setSavePath(getcwd() . "//" . uniqid());
+        // 生成各个接口的文档
+        foreach ($list as $key => $parameter) {
+            $word->create($parameter, $key, $global_paras);
+        }
+        // 下载文件
+        $this->compressAndDownloadFiles($word);
+    }
+
+    /**
+     * @param $word
+     */
+    protected function compressAndDownloadFiles($word)
+    {
+        // 压缩处理
+        $compressService = new CreateZipArchiveService();
+        // 设置压缩文件的名称
+        $compressService->setCompressFile(getcwd() . '/zip/' . uniqid() . '.zip');
+        // 压缩目录中的文件
+        $compressService->compress($word->getSavePath());
+        // 删除目录及其文件
+        $compressService->deleteFiles($word->getSavePath());
+        // 下载文件
+        $compressService->downloadCompressFile();
+    }
+
 }
 
-// 压缩处理
-$compressService = new \Pis0sion\src\service\CreateZipArchiveService();
-// 设置压缩文件的名称
-$compressService->setCompressFile(getcwd() . '/zip/' . uniqid() . '.zip');
-// 压缩目录中的文件
-$compressService->compress($word->getSavePath());
-// 删除目录及其文件
-$compressService->deleteFiles($word->getSavePath());
-// 下载文件
-var_dump($compressService->getCompressFile());
+
+
+
 
 
 
